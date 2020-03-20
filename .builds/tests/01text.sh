7721331c661ec220ad04557ca325ec65b486038c
Sheldon Rupp <sheldon.rupp@epitech.eu>
2020-03-20 18:46:54 +0100

    Add set -xe

--- .builds/tests/01text.sh
+++ .builds/tests/01text.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+set -x
+set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 $DIR/../../builds/disphord webhook:send --text "Test Text"
--- .builds/tests/02embed.sh
+++ .builds/tests/02embed.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -x
+set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 $DIR/../../builds/disphord webhook:send \
     --title "Title" \
