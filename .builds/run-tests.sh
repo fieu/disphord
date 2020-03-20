237c6a4ea142b84a7ccd9df8699bcfb5380e66fd
Sheldon Rupp <sheldon.rupp@epitech.eu>
2020-03-20 19:46:01 +0100

    Remove verbose printing

--- .builds/run-tests.sh
+++ .builds/run-tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -x
-set -e
+# set -x
+# set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 for file in $DIR/tests/*.sh; do echo "Executing $(basename $file)..." && command "$file"; done
