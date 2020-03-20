8df698a40cc735491aefd0f3065f81c726a166b7
Sheldon Rupp <sheldon.rupp@epitech.eu>
2020-03-20 19:00:02 +0100

    Add PHP version

--- .builds/tests/01text.sh
+++ .builds/tests/01text.sh
@@ -2,4 +2,6 @@
 set -x
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-$DIR/../../builds/disphord webhook:send --text "Test Text"
+$DIR/../../builds/disphord webhook:send  \
+    --username "$(php --version | cut -d'(' -f 1 | head -n1)" \
+    --text "Text"
