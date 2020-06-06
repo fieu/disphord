8d3538c83c2ca057a6ed922143a9cd0548e52cb6
Sheldon Rupp <me@suce.dev>
2020-06-06 16:25:58 +0200

    Rename to GitHub Actions

--- .builds/tests/00env-info.sh
+++ .builds/tests/00env-info.sh
@@ -3,7 +3,7 @@ set -x
 set -e
 $TEST_DIR/../../builds/disphord webhook:send -v \
     --username "$(php -r 'echo "PHP " . phpversion();')" \
-    --title "builds.sr.ht - Build environment" \
+    --title "GitHub Actions - Build environment" \
     --description "Build Number: #$GITHUB_RUN_ID" \
     --field "Distribution, \`$(lsb_release -i | cut -d':' -f2 | xargs)\`, true" \
     --field "Version, \`$(lsb_release -c | cut -d':' -f2 | xargs) ($(lsb_release -r | cut -d':' -f2 | xargs))\`, true" \
