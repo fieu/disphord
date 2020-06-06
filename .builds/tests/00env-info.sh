04c5f514385cd57201106c5761ed654f7836095b
Sheldon Rupp <me@suce.dev>
2020-06-06 16:23:13 +0200

    Change for GitHub Actions

--- .builds/tests/00env-info.sh
+++ .builds/tests/00env-info.sh
@@ -4,10 +4,10 @@ set -e
 $TEST_DIR/../../builds/disphord webhook:send -v \
     --username "$(php -r 'echo "PHP " . phpversion();')" \
     --title "builds.sr.ht - Build environment" \
-    --description "Build URL: $JOB_URL" \
+    --description "Build Number: #$GITHUB_RUN_ID" \
     --field "Distribution, \`$(lsb_release -i | cut -d':' -f2 | xargs)\`, true" \
     --field "Version, \`$(lsb_release -c | cut -d':' -f2 | xargs) ($(lsb_release -r | cut -d':' -f2 | xargs))\`, true" \
     --field "Kernel, \`$(uname -r)\`, true" \
     --field "Architecture, \`$(hostnamectl | grep Architecture | cut -d':' -f2 | xargs)\`, true" \
     --field "Hostname, \`$(hostname -f)\`, true" \
-    --field "Job ID, \`#$JOB_ID\`, false" \
+    --field "Job Number, \`#$GITHUB_RUN_NUMBER\`, false" \
