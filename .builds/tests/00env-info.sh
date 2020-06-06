99176902bd13c4789ec2e1ca63097d3eb6b7720e
Sheldon Rupp <me@suce.dev>
2020-06-06 16:24:37 +0200

    Fix up hostname?

--- .builds/tests/00env-info.sh
+++ .builds/tests/00env-info.sh
@@ -9,5 +9,5 @@ $TEST_DIR/../../builds/disphord webhook:send -v \
     --field "Version, \`$(lsb_release -c | cut -d':' -f2 | xargs) ($(lsb_release -r | cut -d':' -f2 | xargs))\`, true" \
     --field "Kernel, \`$(uname -r)\`, true" \
     --field "Architecture, \`$(hostnamectl | grep Architecture | cut -d':' -f2 | xargs)\`, true" \
-    --field "Hostname, \`$(hostname -f)\`, true" \
+    --field "Hostname, \`$(hostname)\`, true" \
     --field "Job Number, \`#$GITHUB_RUN_NUMBER\`, false" \
